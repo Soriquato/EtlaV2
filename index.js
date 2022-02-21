1405294a5a31c91d87efa9ff58f801fc53b45c41
Soriquato <maxence.valere@gmail.com>
2022-02-21 13:22:26 +0100

    Feat: Etla class done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Etla from './src/class/Elta.js'
-import Discord from 'discord.js'
 
 const etla = new Etla()
 
 etla.login("Nzc1Mjk2OTc3MzAyNDU0MzAy.X6kRkw.ddOMN6g2ueFMy1QpdgVjZmWGgPU")
+
+export default etla
