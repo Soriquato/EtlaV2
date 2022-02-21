1100a316ee12469b244c6b18a4cadd78588bfb4c
Soriquato <maxence.valere@gmail.com>
2022-02-21 17:29:57 +0100

    Feat: Command Handler in class

--- src/class/Elta.js
+++ src/class/Elta.js
@@ -1,14 +1,16 @@
 import Discord, { Intents } from 'discord.js';
 import Logger from "../log/Logger.js";
 import fs from 'fs'
+import MessageLoger from '../log/MessageLoger.js';
 
 export default class Etla extends Discord.Client {
     constructor(){
         super({
-            intents: [Intents.FLAGS.GUILD_MESSAGES],
+            intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILDS],
             allowedMentions: { parse: ['users'] },
         });
         this.logger = new Logger()
+        this.messageLoger = new MessageLoger()
         this.commands = []
         this.events = []
         this.loadCommands()
@@ -18,6 +20,23 @@ export default class Etla extends Discord.Client {
         })
         this.on('error', (error) => {
             this.logger.error(error.message)
+        })
+        this.on('messageCreate', async (message) => {
+            let messageArray = message.content.split(" "); 
+            let prefix = "!"
+            if(messageArray[0][0] === prefix){
+                let cmd = messageArray[0].slice(1);
+                let args = message.content.slice(0).split(' '); 
+                if(this.commands.includes(cmd)) {
+                    try {
+                        let command = await import(`../commandes/${cmd}.js`);
+                        await command.execute(message, ...args);
+                    } catch (error) {
+                        this.logger.error(error.message);
+                        message.reply({embeds: [this.messageLoger.errorMessage(error.message)]})
+                    }
+                }
+            } 
         })
     }
 
